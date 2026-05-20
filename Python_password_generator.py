6d875d9d30e93a77684d163cf56b414253c66273
NIHALsinghUs <nihalsingh21072007@gmail.com>
2026-05-20 18:47:25 +0530

    Python_password_generator.py

--- Python_password_generator.py
+++ Python_password_generator.py
@@ -2,10 +2,7 @@
 import random
 
 # List containing alphabets, numbers and special characters
-char = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-        '0','1','2','3','4','5','6','7','8','9',
-        '!','@','#','$','%','^','&','*']
+char = list(string.ascii_letters + string.digits + "!@#$%^&*")
 
 # Empty list to store password characters
 password = []
